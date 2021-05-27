3a9497d9a93b5cbd6ae37aad585b1812650080fe
nestor-c <Nestord.cortes@gmail.com>
2021-05-26 23:02:13 -0700

    Good start to get the brain churning

---
+++ Renderer.cpp
@@ -0,0 +1,4 @@
+#include "Renderer.h"
+Renderer::Renderer(int myScale):cols(64),rows(32),scale(myScale){
+    //Needs a HTML5 canvas equivalent. Look into SDL or SDL2
+};
---
+++ Renderer.h
@@ -0,0 +1,13 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+class Renderer{
+    public:
+        Renderer(int);
+    private:
+        int cols;
+        int rows;
+        int scale;
+
+};
+#endif

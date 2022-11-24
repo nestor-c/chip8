4b2b02d455180f250fbff254f992ef768392448f
nestor-c <nestord.cortes@gmail.com>
2022-11-23 21:30:49 -0800

    Fixed renderer

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -46,7 +46,7 @@ Renderer::Renderer(int myScale):cols(64),rows(32),scale(myScale),windowWidth(col
         SDL_GetError()
     );
 
-	texture = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING,windowWidth,windowHeight);
+	texture = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,windowWidth,windowHeight);
 };
 
 void Renderer::freeResources(){
@@ -68,7 +68,7 @@ bool Renderer::setPixel(int x,int y){
     }
     int pixelLoc = x + (y*cols);
 	
-	// 4294967295 -> in Binary (11111111 11111111 11111111 11111111)
+	// 4294967295 -> in Binary (11111111 11111111 11111111 11111111) which is one unit of a Uint32
     display[pixelLoc] ^= (Uint32)4294967295;
 
     return !display[pixelLoc];
@@ -80,23 +80,22 @@ void Renderer::clear(){
 
 void Renderer::render(){
     while(!quit){
-        // CHECK_ERROR(
-        //     SDL_SetRenderDrawColor(renderer,255,255,255,255) < 0,
-        //     SDL_GetError()
-        //     );
-        
-        // CHECK_ERROR (
-        //     SDL_RenderClear(renderer)< 0, 
-        //      SDL_GetError()
-        // );
-        testRender();
-        SDL_UpdateTexture(texture,NULL,display,windowWidth*sizeof(Uint32));
-        //SDL_RenderCopy(renderer,texture,NULL,NULL);
+        SDL_SetRenderDrawColor(renderer,255,255,255,255);
+        SDL_RenderClear(renderer);
+        display[1056]=0;     
+        for(int i=0;i<rows*cols;i++){
+            if(!display[i]){
+                x = (i%cols)*scale;
+                y = floor(i/cols)*scale;
+                SDL_Rect pixelFill = {x,y,scale,scale};
+                SDL_SetRenderDrawColor(renderer,0,255,0,255);
+                SDL_RenderFillRect(renderer,&pixelFill);
+            }
+        }
         SDL_RenderPresent(renderer);
-        SDL_Event event;
         
+        SDL_Event event;
         while(SDL_PollEvent(&event)){
-            
             switch(event.type){
                 case SDL_QUIT:
                     quit=true;
@@ -111,6 +110,6 @@ void Renderer::render(){
 
 void Renderer::testRender(){
     setPixel(320,160);
-	setPixel(321,160);
-	setPixel(322,160);
+    setPixel(321,160);
+    setPixel(322,160);
 }

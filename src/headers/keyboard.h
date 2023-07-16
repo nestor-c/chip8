bf9eb470a179e1bdf7a60ccaec777e3dd2b987cb
nestor-c <nestord.cortes@gmail.com>
2023-07-15 21:41:42 -0700

    "Debugging STL MAP"

--- src/headers/keyboard.h
+++ src/headers/keyboard.h
@@ -1,15 +1,12 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 #include <iostream>
-#include <map>
 #include <SDL2/SDL.h>
 #include <functional>
 
 class Keyboard{
 	public:
     Keyboard();
-    std::map<SDL_Scancode,int> KEYMAP;
-	std::map<int,bool> keysPressed;
 	std::function<void(int)> onNextKeyPress;
 	bool isKeyPressed(uint8_t m_Key);
 	void onKeyDown(SDL_Event event);
